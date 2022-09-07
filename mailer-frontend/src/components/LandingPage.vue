1744988fbe67873ca2933d1d5d78f65a45d3256f
Braydon Viragh <bviragh@creativeguild.ca>
2022-09-07 11:57:34 +0200

    instructions fix

--- mailer-frontend/src/components/LandingPage.vue
+++ mailer-frontend/src/components/LandingPage.vue
@@ -3,7 +3,7 @@
         <div class="page-container">
             <div class="page-title">
                 <h1>Welcome to the Chuck Norris Challenge</h1>
-                <p>Enter emails in the box provided and end every email with a comma , these addresses will receive a dynamically rendered Chuck norris joke from an API</p>
+                <p>Enter emails in the box provided and seperate every email with a SPACE these addresses will receive a dynamically rendered Chuck norris joke from an API</p>
             </div>
             <EmailForm/>
         </div>
